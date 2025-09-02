296a2754fd9c9c5a1050805543cdc51d9928caad
Hashforgamers <hashforgamers@gmail.com>
2025-09-02 08:57:53 +0000

    Fixed the docket synamic varibale

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,4 @@ export const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL!;
 export const USER_ONBOARD_URL = process.env.NEXT_PUBLIC_USER_ONBOARD!;
 export const VENDOR_ONBOARD_URL = process.env.NEXT_PUBLIC_VENDOR_ONBOARD!;
 export const BOOKING_URL = process.env.NEXT_PUBLIC_BOOKING_URL!;
+export const SOCKET_DASHBOARD_URL= process.env.NEXT_PUBLIC_SOCKET_BOOKING_URL
